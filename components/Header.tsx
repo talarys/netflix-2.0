7d8f0d32edf4119d5c61758cac43d3b981318492
visotoniki9 <d.simon.ssx0@gmail.com>
2022-06-20 20:30:43 +0200

    Removed hidden tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,7 +41,7 @@ function Header() {
         </ul>
       </div>
       <div className="flex items-center space-x-4 text-sm font-light">
-        <SearchIcon className="hidden sm:inline h-6 w-6" />
+        <SearchIcon className="h-6 w-6" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
         <Link href="/account">
