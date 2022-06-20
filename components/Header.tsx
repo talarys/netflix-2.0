851dfc8c8cc202b30f803dca801856ae05a3fe77
visotoniki9 <d.simon.ssx0@gmail.com>
2022-06-20 20:23:58 +0200

    Mostly finished header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BellIcon, SearchIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div>Header</div>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          className="cursor-pointer object-contain"
+          width={100}
+          height={100}
+          alt="logo"
+          src="/Netflix_logo.svg"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My list</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden sm:inline h-6 w-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img className="cursor-pointer rounded" src="/user.png" alt="user" />
+        </Link>
+      </div>
+    </header>
   );
 }
 
