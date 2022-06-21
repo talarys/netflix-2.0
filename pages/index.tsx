4a38ef1cdb55fdf0968217e541afa2166646a219
visotoniki9 <d.simon.ssx0@gmail.com>
2022-06-21 15:28:04 +0200

    Finished rows

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import Row from '../components/Row';
 import { Movie } from '../typings';
 import requests from '../utils/request';
 
@@ -26,7 +27,7 @@ const Home: NextPage = ({
   topRated,
   trendingNow,
 }:Props) => (
-  <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+  <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
     <Head>
       <title>Netflix</title>
       <link rel="icon" href="/images/favicon.ico" />
@@ -34,11 +35,15 @@ const Home: NextPage = ({
     <Header />
     <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
       <Banner netflixOriginals={netflixOriginals} />
-      <section>
-        {/* Row */}
-        {/* Row */}
-        {/* Row */}
-        {/* Row */}
+      <section className="md:space-y-24">
+        <Row title="Trending Now" movies={trendingNow} />
+        <Row title="Top Rated" movies={topRated} />
+        <Row title="Action Thrillers" movies={actionMovies} />
+        {/* TODO: My list */}
+        <Row title="Comedies" movies={comedyMovies} />
+        <Row title="Scary Movies" movies={horrorMovies} />
+        <Row title="Romance Movies" movies={romanceMovies} />
+        <Row title="Documentaries" movies={documentaries} />
       </section>
     </main>
     {/* Modal */}
