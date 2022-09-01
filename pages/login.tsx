995426b81f0383fc64b44bd4cded07b671f3b0c5
Talarys <d.simon.ssx0@gmail.com>
2022-09-01 19:28:31 +0200

    Fixed small typo

--- pages/login.tsx
+++ pages/login.tsx
@@ -23,9 +23,7 @@ function Login() {
     if (login) {
       await signIn(email, password);
     } else {
-      (
-        await signUp(email, password)
-      );
+      await signUp(email, password);
     }
   };
 
