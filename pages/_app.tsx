f28dac3f4fa7cf7ec7e0f24fe24ed8917dafa93b
Talarys <d.simon.ssx0@gmail.com>
2022-09-01 19:28:09 +0200

    Added recoil and temp modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 import { AuthProvider } from '../hooks/useAuth';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <RecoilRoot>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </RecoilRoot>
   );
 }
 
