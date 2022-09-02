012536f7447ab30d16dccc6a0e9652485ba24214
Talarys <d.simon.ssx0@gmail.com>
2022-09-02 19:05:49 +0200

    Midway modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,21 @@
-import { XIcon } from '@heroicons/react/outline';
+import {
+  PlusIcon, ThumbUpIcon, VolumeOffIcon, VolumeUpIcon, XIcon,
+} from '@heroicons/react/outline';
 import MuiModal from '@mui/material/Modal';
 import { useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
+import ReactPlayer from 'react-player/lazy';
+import { FaPlay } from 'react-icons/fa';
 import { modalState, movieState } from '../atoms/modalAtom';
-import { Movie } from '../typings';
+import { Genre, Movie } from '../typings';
 import { fetchMovieTrailer } from '../utils/request';
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
   const currentMovie = useRecoilValue<Movie|null>(movieState);
   const [trailer, setTrailer] = useState(null);
+  const [genres, setGenres] = useState<Genre[]>(null);
+  const [muted, setMuted] = useState(true);
 
   function handleClose() {
     setShowModal(false);
@@ -20,24 +26,73 @@ function Modal() {
 
     (async () => {
       const data = await fetchMovieTrailer(currentMovie).then((r) => r.json());
-      setTrailer(data);
+
+      const trailers = data?.videos?.results.filter(
+        (t) => t.type === 'Trailer',
+      );
+
+      setTrailer(trailers.length ? trailers[0] : data?.videos?.results[0]);
+
+      if (data?.genres) setGenres(data.genres);
     })();
   }, [currentMovie]);
 
+  if (!trailer) return null;
   return (
-    <MuiModal open={showModal} onClose={handleClose}>
-      <div className="relative h-full bg-[black] mt-7 mx-5 rounded-t-lg p-5">
+    <MuiModal
+      className="fixed !top-7 left-0 right-0 mx-auto max-w-6xl overflow-hidden overflow-y-scroll scrollbar-hide"
+      open={showModal}
+      onClose={handleClose}
+    >
+      {/* Modal Container */}
+      <div className="relative mx-5 overflow-hidden rounded-xl">
+        {/* CloseButton */}
         <button
           onClick={handleClose}
           className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none
           bg-[#181818] hover:bg-[#181818]"
         >
-          <XIcon className="h-6 w-  6" />
+          <XIcon className="h-6 w-6" />
         </button>
-        <h1>{currentMovie?.title}</h1>
-        <h1>{currentMovie?.original_name}</h1>
-        <h1>{trailer?.id}</h1>
-
+        {/* Player */}
+        <div className="relative pt-[56.25%]">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer.key}`}
+            width="100%"
+            height="100%"
+            style={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              pointerEvents: 'none',
+            }}
+            controls={false}
+            playing
+            loop
+            muted={muted}
+          />
+        </div>
+        {/* Buttons */}
+        <div className="absolute bottom-5 flex w-full items-center justify-between px-10">
+          <div className="flex space-x-2">
+            {/* PlayButton */}
+            <button className="font-semibold px-9 py-2 flex items-center gap-x-2 rounded bg-white text-black transition hover:bg-[#e6e6e6]">
+              <FaPlay className="w-6 h-6" />
+              Play
+            </button>
+            <button className="modalButton">
+              <PlusIcon className="h-7 w-7" />
+            </button>
+            <button className="modalButton">
+              <ThumbUpIcon className="h-7 w-7" />
+            </button>
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted
+                ? <VolumeOffIcon className="w-7 h-7" />
+                : <VolumeUpIcon className="w-7 hs-7" /> }
+            </button>
+          </div>
+        </div>
       </div>
     </MuiModal>
   );
