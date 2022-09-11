489123f35a073c749362ed4cd37e6b9965b7ea19
Talarys <d.simon.ssx0@gmail.com>
2022-09-11 21:51:36 +0200

    Fixed some type checks

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,11 +1,12 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
+import { DocumentData } from 'firebase/firestore';
 import { useRef, useState } from 'react';
 import { Movie } from '../typings';
 import Thumbnail from './Thumbnail';
 
 interface Props {
   title: string
-  movies: Movie[]
+  movies: Movie[] | DocumentData
 }
 
 function Row({ title, movies }: Props) {
@@ -40,7 +41,7 @@ function Row({ title, movies }: Props) {
           className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
           ref={rowRef}
         >
-          {movies.map((movie) => (
+          {movies.map((movie : Movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
